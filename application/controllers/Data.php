80a43549ee7425b9c3979121b5c89789ded5eb99
agilcrud <azrilhariansyah4@gmail.com>
2024-01-03 17:04:41 +0700

    first commit

---
+++ application/controllers/Data.php
@@ -0,0 +1,8 @@
+<?php
+class Data extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('DataBarang/index.php');
+	}
+}
---
+++ application/views/home/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/badhabits.css">
+    <title>Home page bad habits</title>
+</head>
+<body>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+     <title> badhabits.</title> 
+    <link rel="stylesheet" href="style.css">
+   </head>
+<body>
+  <nav>
+    <div class="nav-content">
+      <div class="logo">
+        <a href="#">BadHabits.</a>
+      </div>
+      <ul class="nav-links">
+        <li><a href="<?= base_url() ?>">Home</a></li>
+        <li><a href="<?= base_url() ?>data">Data Barang</a></li>
+      </ul>
+    </div>
+  </nav>
+</body>
+</html>
+</body>
+</html>
